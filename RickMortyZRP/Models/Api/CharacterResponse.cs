d5e5fe9580d903a076025914078a1d7153fb9139
Rafaelaltieris <raphaelaltieris@gmail.com>
2025-12-16 10:03:41 -0300

    feat: add api response models and external dtos for rick and morty api

---
+++ RickMortyZRP/Models/Api/CharacterResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace RickMortyZRP.Models.Api
+{
+    public class CharacterResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Status { get; set; } = "";
+        public string Species { get; set; } = "";
+        public string Image { get; set; } = "";
+    }
+}
