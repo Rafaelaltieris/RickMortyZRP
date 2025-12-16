7000f06ccf4be0f2f3c76b480b19a1f6af2cdf09
Rafaelaltieris <raphaelaltieris@gmail.com>
2025-12-16 10:10:44 -0300

    feat: add episodes endpoints

---
+++ RickMortyZRP/Controllers/EpisodesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RickMortyZRP.Services;
+
+namespace RickMortyZRP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EpisodesController : ControllerBase
+    {
+        private readonly IEpisodeService _service;
+
+        public EpisodesController(IEpisodeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int id, CancellationToken ct)
+        {
+            var result = await _service.GetEpisodeWithCharactersAsync(id, ct);
+            if (result is null) return NotFound(new { message = "Episode not found" });
+
+            return Ok(result);
+        }
+    }
+}
